03aa3f289936cbbb6f0452a80f73968b8891d03d
KuGox <kugox@outlook.com>
2015-11-02 17:13:37 +0800

    test3

---
+++ user.py
@@ -0,0 +1,12 @@
+__author__ = 'Kugox'
+import web
+
+urls = ('/user', 'user')
+class user:
+    def GET(self):
+        return "user!"
+
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
